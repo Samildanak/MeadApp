bb743edde05aa32e1462f8a9ca893badde4c4733
samildanak <robin.levivier@gmail.com>
2024-10-26 12:55:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+def calculate_honey(V:float, Cv:float, Cm:float):
+    #M = V * (24.5 * Cv + Cm)
+    return V * (24.5 * Cv + Cm)
+
+def calculate_alcool_brix(Bi:float, Bf:float):
+    #ABV = (Bi - Bf) * 0.59
+    return (Bi - Bf) * 0.59
+
+def calculate_alcool_density(df:float, di:float):
+    #ABV = (di - df) * 131.25
+    return (di - df) * 131.25
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
