7d4ee8956d2cfc61624f037f2e521514d997fa84
samildanak <robin.levivier@gmail.com>
2024-10-26 15:06:15 -0400

    Implement First frame of new recipe window

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from windowsApp import MainWindow
 
-def calculate_honey(V:float, Cv:float, Cm:float):
-    #M = V * (24.5 * Cv + Cm)
-    return V * (24.5 * Cv + Cm)
-
-def calculate_alcool_brix(Bi:float, Bf:float):
-    #ABV = (Bi - Bf) * 0.59
-    return (Bi - Bf) * 0.59
-
-def calculate_alcool_density(df:float, di:float):
-    #ABV = (di - df) * 131.25
-    return (di - df) * 131.25
-
 def main():
     pass
 
--- windowsApp.py
+++ windowsApp.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from datetime import datetime
 
 class MainWindow(tk.Tk):
     def __init__(self):
@@ -6,11 +7,11 @@ class MainWindow(tk.Tk):
         self.title("Super Mead App")
         self.geometry("300x200")
 
-        btn_new_recipe = tk.Button(self, text="New Recipe", command=self.open_new_recipe)
+        btn_new_recipe = tk.Button(self, text="New Recipe", command=self.open_new_recipe, width=20, height=2)
         btn_new_recipe.pack(pady=40)
 
-        btn_show_recipe = tk.Button(self, text="Show Recipes", command=self.open_show_recipe)
-        btn_show_recipe.pack(pady=20)
+        btn_show_recipe = tk.Button(self, text="Show Recipes", command=self.open_show_recipe, width=20, height=2)
+        btn_show_recipe.pack()
 
     def open_new_recipe(self):
         NewRecipeWindow(self)
@@ -19,13 +20,52 @@ class MainWindow(tk.Tk):
         ShowRecipeWindow(self)
 
 class NewRecipeWindow(tk.Toplevel):
+    # A New Recipe should provide :
+    #   - Name (Text Box)
+    #   - Batch Id (Label auto calculated)
+    #   - Date (Label auto provided)
+    #   - Honey Quantity (Label auto calculated)
+    #   - yeast type (Selection from yeast available)
+    #   - use of K Fermaid (Check box)
+    #   - Alcohol predict (Text box)
+    #   - Volume prepared (Text box)
+    #   - Residual Sugar wanted (Text box)
+    #   - Table for Residual Sugar to help (Label)
+    #   - Note page (Text box)
+    #   - Save Button (Button)
+
     def __init__(self, parent):
         super().__init__(parent)
         self.title("New Recipe")
-        self.geometry("200x150")
+        self.geometry("600x800")
 
-        label = tk.Label(self, text="New Recipe Window")
-        label.pack(pady=20)
+        id = self.get_id()
+        date = datetime.now().strftime("%d/%m/%Y")
+
+        self.name_id_frame = tk.Frame(self)
+        self.ingredients_frame = tk.Frame(self)
+        self.residual_frame = tk.Frame(self)
+        self.note_frame = tk.Frame
+        self.button_frame = tk.Frame(self)
+
+        self.name_id_frame.pack()
+
+        self.batch_lbl = tk.Label(self.name_id_frame, text=("Batch n° " + str(id)))
+
+        self.name_lbl = tk.Label(self.name_id_frame, text="Name : ")
+        self.name_txt_box = tk.Text(self.name_id_frame, height=1, width=40)
+
+        self.date_lbl = tk.Label(self.name_id_frame, text="Date")
+        self.date_calc_lbl = tk.Label(self.name_id_frame, text=date)
+
+        self.batch_lbl.grid(column=0, row=0)
+        self.name_lbl.grid(column=1, row=1)
+        self.name_txt_box.grid(column=2, row=1)
+        self.date_lbl.grid(column=1, row=2)
+        self.date_calc_lbl.grid(column=2, row=2)
+
+    def get_id(self):
+        return 1
 
 class ShowRecipeWindow(tk.Toplevel):
     def __init__(self, parent):
